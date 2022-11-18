20f66ef700587ba899c16a9cda7f0e481994925b
shetara2022 <smithshetarall@gmail.com>
2022-11-17 21:28:33 -0500

    added jwt authentication

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useHistory } from "react-router"
+// import { useHistory } from "react-router"
 
 function NewCommentForm({ place, onSubmit }) {
 
--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react"
-import { useHistory, useParams } from "react-router"
+import { useState } from "react"
+import { useHistory } from "react-router"
+// import { useEffect } from "react"
+// import {useParams} from "react-router"
 
 function SignUpForm() {
 
