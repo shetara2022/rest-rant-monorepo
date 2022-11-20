5017906e0c067aaf4e3cd8277689f1cfebb1b2a8
shetara2022 <smithshetarall@gmail.com>
2022-11-20 11:59:09 -0500

    Rest-Rant comments with authentication

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 // import { useHistory } from "react-router"
+import { CurrentUser } from '../contexts/CurrentUser'
 
 function NewCommentForm({ place, onSubmit }) {
 
@@ -36,6 +37,11 @@ function NewCommentForm({ place, onSubmit }) {
             authorId: authors[0]?.userId
         })
     }
+    const { currentUser } = useContext(CurrentUser)
+
+    if (!currentUser) {
+        return <p>You must be logged in to leave a rant or rave!</p>
+    }
 
     return (
         <form onSubmit={handleSubmit}>
@@ -53,12 +59,12 @@ function NewCommentForm({ place, onSubmit }) {
                 </div>
             </div>
             <div className="row">
-                <div className="form-group col-sm-4">
+                {/* <div className="form-group col-sm-4">
                     <label htmlFor="state">Author</label>
                     <select className="form-control" value={comment.authorId} onChange={e => setComment({ ...comment, authorId: e.target.value })}>
                         {authorOptions}
                     </select>
-                </div>
+                </div> */}
                 <div className="form-group col-sm-4">
                     <label htmlFor="stars">Star Rating</label>
                     <input
